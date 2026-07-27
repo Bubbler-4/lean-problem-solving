e742e2a7701388697c5abb028ed9096612b1c9b5
Bubbler <bubbler9903@gmail.com>
2026-07-28 08:23:36 +0900

    Add Mispelling 2

---
+++ examples/Mispelling2.lean
@@ -0,0 +1,20 @@
+import ProblemSolving
+
+/-!
+# Mispelling 2
+
+For each test case, remove the $N$-th (one-based) character from a string $S$.
+
+* Input: the number of test cases $T$ followed by $T$ test cases.
+  * Each test case is a single line containing $N$ and $S$ separated by whitespace.
+  * $S$ consists of ASCII alphanumeric characters and spaces.
+  * $1 ≤ N ≤ |S|$; $|S| ≥ 2$.
+* Output: The answer for each test case on its own line.
+-/
+
+def main := mainWrapper (Σ t : Nat, Vector (Nat × ByteArrayNL) t) fun ⟨_, testcases⟩ => do
+  for ⟨index, string⟩ in testcases do
+    let string := (string : ByteArray)
+    let left := string.extract 0 (index - 1)
+    let right := string.extract index string.size
+    IO.println s!"{String.fromUTF8! (left.append right)}"
